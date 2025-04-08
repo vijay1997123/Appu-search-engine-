cd0775cf27c225be2ce2761a25a31e30f8800083
vijay1997123 <64305571+vijay1997123@users.noreply.github.com>
2025-04-08 21:41:04 +0530

    Initial source code upload
    Initial source code upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    results = []
+    if request.method == 'POST':
+        query = request.form['query']
+        results = search_web(query)
+    return render_template('index.html', results=results)
+
+def search_web(query):
+    # Dummy search results (real crawler will be added later)
+    return [
+        {'title': 'Example Site 1', 'link': 'https://example.com/1', 'desc': 'This is a sample result'},
+        {'title': 'Example Site 2', 'link': 'https://example.com/2', 'desc': 'Another example site'},
+    ]
+
+if __name__ == '__main__':
+    app.run(debug=True)
