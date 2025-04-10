647630bdbc340ff976f6038a3d23232c1fec8750
vijay1997123 <64305571+vijay1997123@users.noreply.github.com>
2025-04-10 15:04:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import search
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    results = []
-    query = ""
-    if request.method == "POST":
-        query = request.form["query"]
-        results = search.search(query)
-    return render_template("index.html", results=results, query=query)
+    return render_template("index.html")
+
+# This is the route your search.html is trying to call
+@app.route("/api/search")
+def api_search():
+    query = request.args.get("q", "")
+    results = search.search_web(query)  # make sure your search.py has this function
+    return jsonify(results)
 
 if __name__ == "__main__":
     app.run(debug=True)
